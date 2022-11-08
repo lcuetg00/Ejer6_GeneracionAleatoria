0bd5586d7ad303ad74120b45a7b1c6f6453c83cb
lcuetg00 <47599827+lcuetg00@users.noreply.github.com>
2022-11-08 12:48:06 +0100

    Actualización incompleta. Falta por completar:
    -La forma de creación de FigurasGeometricas en consola (identifica su clase y crea un objeto de esa clase con los valores pasados por parámetro).
    -Método para imprimir las características de un Triangulo, Cuadrado, Circulo en consola.
    -Crear un método que identifique la clase de la figura e imprima sus características.
    -Calcular el área de un Triangulo de la forma correcta.
    -Comprobación de nulls, etc.

---
+++ src/Cuadrilateros/Cuadrilateros.java
@@ -0,0 +1,39 @@
+package Figuras.FiguraGeometrica.Cuadrilateros;
+import Figuras.FiguraGeometrica.FiguraGeometrica;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase para la implementación de objetos geométricos pertenecientes a los cuadriláteros.
+ * Todos los cuadriláteros tienen en común:
+ * -Tienen 4 lados
+ * -Forma de cacular su perímetro.
+ */public abstract class Cuadrilateros implements FiguraGeometrica {
+    /**
+     * Lista que contendrá los 4 lados de un cuadrilátero
+     */
+    protected List<BigDecimal> lados;
+    /**
+     * Representa el número de lados que tiene un cuadrilátero
+     */
+    public static final int NUM_LADOS_CUADRILATERO = 4;
+
+    /**
+     * Devuelve el perímetro con la forma de calcularla para los cuadriláteros
+     * Perímetro = suma de todos sus lados
+     * @return
+     */
+    @Override
+    public BigDecimal calcularPerimetro() {
+        BigDecimal perimetro = BigDecimal.ZERO;
+        for(int i=0;i<lados.size();i++) {
+            perimetro = perimetro.add(lados.get(i));
+        }
+        return perimetro;
+    }
+
+
+
+}
