bdd3d235415b9d74a1a1504fa1e2bb083c9b74cb
lcuetg00 <47599827+lcuetg00@users.noreply.github.com>
2022-11-21 19:57:54 +0100

    Actualizado

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,6 @@ import com.ejer.figura.cuadrilatero.Cuadrado;
 public class Main {
 
     public static void main(String[] args) {
-        Cuadrado cuadrado = new Cuadrado(0,10,2);
-
-        System.out.println(cuadrado);
         Consola consola = new Consola();
         consola.iniciarConsola();
     }
